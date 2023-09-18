9ae9dbe1ded2de23dd6ab75f587ad11537910d76
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2023-09-18 15:29:41 +0200

    remise au propre

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,24 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smarco <smarco@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/02 14:03:19 by smarco            #+#    #+#             */
-/*   Updated: 2023/02/03 15:42:04 by smarco           ###   ########.fr       */
+/*   Created: 2022/11/02 14:03:30 by smarco            #+#    #+#             */
+/*   Updated: 2023/01/31 21:15:15 by smarco           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
-int	ft_printf(const char *str, ...)
+int	ft_putchar(int c)
 {
-	va_list	arg;
-	int		result;
-
-	result = 0;
-	va_start(arg, str);
-	result = ft_check(arg, str, result);
-	va_end(arg);
-	return (result);
+	if (write(1, &c, 1) != 1)
+		return (-1);
+	return (1);
 }
